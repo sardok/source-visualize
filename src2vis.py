e2ebc460985da6055a04e62ad9de8a9a14506987
sinan <sinan@ev>
2013-01-18 19:32:07 +0200

    Changed database.

--- src2vis.py
+++ src2vis.py
@@ -6,10 +6,11 @@
 #
 
 import sys
-import networkx as nx
 from re import match
+from copy import deepcopy
 from os import getcwd, remove
 from os.path import basename, isfile
+import networkx as nx
 
 def create_graph(syms):
     graph = nx.Graph()
@@ -35,7 +36,67 @@ def create_graph(syms):
 
 def create_symbol_table(input_file):
     sym_table = {}
-    cscope_line = { 'file':'', 'func':'', 'xref':'' }
+    cur_refs = {'index':[], 'func':''}
+
+    def dict_merge(a, b):
+        if not isinstance(b, dict):
+            return b
+        res = deepcopy(a)
+        for k,v in b.iteritems():
+            if k in res and isinstance(res[k], dict):
+                res[k] = dict_merge(res[k], v)
+            else:
+                res[k] = deepcopy(v)
+        return res
+
+    def update(entry, val):
+        if isinstance(entry, dict):
+            entry.update(val)
+        elif isinstance(entry, list):
+            entry.append(val)
+        elif isinstance(entry, set):
+            entry.add(val)
+        else:
+            entry = val
+        
+    def deep_update(db, keys, val):
+        key = keys[0]
+        if keys[1:].__len__() > 0:
+            deep_update(db[key], keys[1:], val)
+        else:
+            update(db[key], val)
+            
+    def deep_get(db, keys):
+        key = keys[0]
+        if keys[1:].__len__() > 0:
+            return deep_get(db[key], keys[1:])
+        else:
+            return db[key]
+        
+    def file_tag_parser(entry):
+        entries = entry.split('/')
+        entries = filter(lambda x: x != '', entries)
+        cur_refs['index'] = deepcopy(entries)
+        cur_refs['func'] = ''
+        entries.reverse()
+        tmp = {}
+        for dir in entries:
+            tmp = {dir:tmp}
+        merged = dict_merge(sym_table, tmp)
+        sym_table.update(merged)
+        
+    def func_tag_parser(entry):
+        if not cur_refs['index']:
+            return
+        deep_update(sym_table, cur_refs['index'], {entry:set([])})
+        cur_refs['func'] = entry
+
+    def xref_tag_parser(entry):
+        if not (cur_refs['index'] and cur_refs['func']):
+            return
+        index = deepcopy(cur_refs['index'])
+        index.append(cur_refs['func'])
+        deep_update(sym_table, index, entry)
 
     def cscope_line_validator(item, regex):
         def parser(line):
@@ -43,60 +104,29 @@ def create_symbol_table(input_file):
             if m: return item, m.groups()[0]
         return parser
 
-    def file_tag_parser(entry):
-        for dir in entry.split('\\'):
-            sym_table[dir] = {}
-
-    def func_tag_parser(entry):
-        return 1
-
-    def xref_tag_parser(entry):
-        return 1
-
-    def insert(cscope_line):
-        file = cscope_line['file']
-        if not file: return
-        if not sym_table.has_key(file):
-            sym_table[file] = {'lines':0, 'defs':{}}
-            return
-
-        sym_table[file]['lines'] += 1
-        func = cscope_line['func']
-        if not func: return
-        if not sym_table[file]['defs'].has_key(func):
-            sym_table[file]['defs'][func] = set([])
-            return
-
-        xref = cscope_line['xref']
-        if not xref: return
-        if not xref in sym_table[file]['defs'][func]:
-            sym_table[file]['defs'][func].add(xref)
-
     validators = [cscope_line_validator('file', '\s@(.*\.[ch])'),
                   cscope_line_validator('func', '\s\$(.*)'),
                   cscope_line_validator('xref', '\s\`(.*)')]
 
-    parsers = {'file':file_tag_parser, 'func':func_tag_parser,
+    parsers = {'file':file_tag_parser,
+               'func':func_tag_parser,
                'xref':xref_tag_parser}
     
     with open(input_file) as f:
-        sym = None
-
-        # First iteration to generate source symbols
         for line in f:
-            parser = filter(lambda x: x(line), parsers)
-            if parser:
-                func = parser[0]
-                key, entry = func(line)
+            valids = filter(lambda x: x(line), validators)
+            if valids:
+                validator = valids[0]
+                key, entry = validator(line)
                 parsers[key](entry)
-                
+    print sym_table
     return sym_table
     
 if __name__ == '__main__':
     cscope_file = sys.argv[1]
     symbols = create_symbol_table(cscope_file)
-    graph = create_graph(symbols)
-    output = basename(getcwd()) + '.gml'
-    if isfile(output):
-        remove(output)
-    nx.write_gml(graph, output)
+    #graph = create_graph(symbols)
+    # output = basename(getcwd()) + '.gml'
+    # if isfile(output):
+    #     remove(output)
+    # nx.write_gml(graph, output)
