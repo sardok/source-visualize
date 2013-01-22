8054a950b144b23ccf7f38e6a7f1a18eb91910ea
Sinan Nalkaya <sardok@gmail.com>
2013-01-22 16:41:18 +0200

    fix dict parent weight overwrite defect. Restarted graph work

--- src2vis.py
+++ src2vis.py
@@ -16,16 +16,16 @@ def create_graph(syms_table):
     graph = nx.Graph()
 
     def iterate_table(entry, parent=None):
+        print entry
         for key in entry:
             if key == 'weight':
                 continue
-            
-            if not entry.has_key('weight'):
-                return
-            
-            print entry['weight']
-            graph.add_node(key, {'weight':entry['weight']})
-            if parent: graph.add_edge(parent, key)
+            if not (isinstance(entry[key], dict) and \
+                        entry[key].has_key('weight')):
+                continue
+            graph.add_node(key, {'weight':entry[key]['weight'],'label':key})
+            if parent:
+                graph.add_edge(parent, key)
             iterate_table(entry[key], key)
             
     iterate_table(syms_table)
@@ -34,17 +34,14 @@ def create_graph(syms_table):
 def create_symbol_table(input_file):
     sym_table = {}
     cur_refs = {'index':[], 'func':''}
-    dbg_counter = 0
 
     def dict_merge(a, b, update_existing=True):
         if not isinstance(b, dict):
             return b
         res = deepcopy(a)
-        print 'update_existing: ' + update_existing.__str__()
         for k,v in b.iteritems():
             if k in res:
                 if isinstance(res[k], dict):
-                    print 'recursion'
                     res[k] = dict_merge(res[k], v, update_existing)
                 elif update_existing == True:
                     res[k] = deepcopy(v)
@@ -77,9 +74,7 @@ def create_symbol_table(input_file):
             return db[key]
             
     def add_weight(db):
-        print 'add_weight '
-        print db
-        db.update(dict_merge(db, {'weight':0}, False))
+        db.update(dict_merge(db, {'weight':0}))
         
     def file_tag_parser(entry):
         entries = entry.split('/')
@@ -89,21 +84,18 @@ def create_symbol_table(input_file):
         entries.reverse()
         tmp = {}
         for dir in entries:
-            print 'calling update_weight for: ' + dir
             add_weight(tmp)
             tmp = {dir:tmp}
-        merged = dict_merge(sym_table, tmp)
+        merged = dict_merge(sym_table, tmp, False)
         sym_table.update(merged)
         
     def func_tag_parser(entry):
-        return
         if not cur_refs['index']:
             return
         deep_update(sym_table, cur_refs['index'], {entry:set([])})
         cur_refs['func'] = entry
 
     def xref_tag_parser(entry):
-        return
         if not (cur_refs['index'] and cur_refs['func']):
             return
         index = deepcopy(cur_refs['index'])
@@ -116,32 +108,17 @@ def create_symbol_table(input_file):
             if m: return item, m.groups()[0]
         return parser
 
-    # This function is called likely every iteration
-    # This function keeps updating 'weight' of each
-    # source file.
+    # This function is called likely every on
+    # iteration and keeps updating 'weight' of
+    # each source file.
     def update_weights():
         refs = []
         for ref in cur_refs['index']:
-            print 'updating ref: ' + ref
             refs.append(ref)
             weight_ref = refs + ['weight']
             weight = deep_get(sym_table, weight_ref)
-            if ref == 'src':
-                print 'src weight: ' + str(weight)
             deep_update(sym_table, weight_ref, weight + 1)
 
-        # if not cur_refs['index']:
-        #     return
-        # try:
-        #     index = deepcopy(cur_refs['index'])
-        #     index.append('weight')
-        #     weight = deep_get(sym_table, index)
-        #     deep_update(sym_table, index, weight + 1)
-        # except KeyError:
-        #     last_entry = deep_get(sym_table, cur_refs['index'])
-        #     merged = dict_merge(last_entry, {'weight':0})
-        #     last_entry.update(merged)
-        
     validators = [cscope_line_validator('file', '\s@(.*\.[ch])'),
                   cscope_line_validator('func', '\s\$(.*)'),
                   cscope_line_validator('xref', '\s\`(.*)')]
@@ -160,24 +137,28 @@ def create_symbol_table(input_file):
                 parsers[key](entry)
 
             update_weights()
-    print sym_table
+    #print sym_table
     return sym_table
     
 if __name__ == '__main__':
     cscope_file = sys.argv[1]
     symbols = create_symbol_table(cscope_file)
-    # graph = create_graph(symbols)
-    # pos = nx.graphviz_layout(graph)
-    # for node in graph.nodes(data=True):
-    #     print node
-    # nodesize = [d['weight'] for (n,d) in graph.nodes(data=True)]
-    # print nodesize
-    # nx.draw_networkx_nodes(graph, pos, node_size=nodesize, node_color='w', alpha=0.4)
-    # plt.figure(figsize=(8,8))
-    # plt.axis('off')
-    # plt.savefig("chess_masters.png",dpi=75)
-    # print("Wrote chess_masters.png")
-    # plt.show() # display
+    graph = create_graph(symbols)
+    pos = nx.graphviz_layout(graph)
+#    for node in graph.nodes(data=True):
+#        print node
+#    print graph.nodes(data=True)
+    print [(d['weight'], d['label']) for (n,d) in graph.nodes(data=True)]
+    #print nodesize
+    #print labels
+    exit(9)
+    nx.draw_networkx_nodes(graph, pos, node_size=nodesize, node_color='w', alpha=0.4)
+    nx.draw_networkx_labels(graph, pos, labels, font_size=16)
+    plt.figure(figsize=(8,8))
+    plt.axis('off')
+    #plt.savefig("chess_masters.png",dpi=75)
+    #print("Wrote chess_masters.png")
+    plt.show() # display
     # output = basename(getcwd()) + '.gml'
     # if isfile(output):
     #     remove(output)
